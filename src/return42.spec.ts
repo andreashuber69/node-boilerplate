02e7127893d8c1cef365168c44c429067de8ee79
Andreas Huber <andreashuber69@users.noreply.github.com>
2023-09-29 23:30:28 +0200

    chore: update packages

--- src/return42.spec.ts
+++ src/return42.spec.ts
@@ -2,8 +2,8 @@ import assert from "node:assert";
 import { describe, it } from "node:test";
 import { return42 } from "./return42.js";
 
-describe("return42", () => {
-    it("should return 42", () => {
+await describe("return42", async () => {
+    await it("should return 42", () => {
         assert(return42() === 42);
     });
 });
