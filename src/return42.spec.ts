b56761825acf56b0414e258991113ba1569c7b6b
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-21 22:58:08 +0100

    chore: minor change

--- src/return42.spec.ts
+++ src/return42.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { return42 } from "./return42.js";
+import { return42 } from "./return42.ts";
 
 describe("return42", () => {
     it("should return 42", () => {
